1afd8ede8354204a347d0805414c30c3e829a0c1
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2026-08-26 10:01:29 +0530

    JS to NB

--- NB/L2/JS/index.js
+++ NB/L2/JS/index.js
@@ -1,4 +1,19 @@
-function changeCircleSize(){
-    let r = document.getElementById("user_inp").value
-    document.getElementById("svg1").innerHTML = `<circle r=${r} cx='250' cy='250' fill='red' />`
+function changeCircleSize() {
+  let res = validation();
+  if (res) {
+    let r = document.getElementById("user_inp").value;
+    document.getElementById("svg1").innerHTML =
+      `<circle r=${r} cx='250' cy='250' fill='red' />`;
+    document.getElementById("res").innerHTML = 3.141 * r * r;
+  }
+}
+function validation() {
+  let r = document.getElementById("user_inp").value;
+  if (r === "") {
+    document.getElementById("err_container").innerHTML =
+      "Error: Radius can't be blank!";
+    document.getElementById("user_inp").style.border = "1px solid red";
+    return false;
+  }
+  return true;
 }
