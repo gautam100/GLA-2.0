7b85353fdf1d31d331fd88032965df56266202a7
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2026-08-28 10:40:13 +0530

    changes

--- NA/JS/L1/circle.js
+++ NA/JS/L1/circle.js
@@ -1,6 +1,17 @@
 function changeSize() {
   let r = document.getElementById("usr_inp").value;
-  document.getElementById("svg1").innerHTML =
+  if(validation(r)){
+    document.getElementById("svg1").innerHTML =
     `<circle r="${r}" cx="250" cy="250" fill="red" />`;
     document.getElementById("circle_area").innerHTML = 3.141 * r * r
+  }
+}
+
+let validation = function(radius){
+  if(radius === ""){
+    document.getElementById("usr_inp").style.border = "1px solid red"
+    document.getElementById("err_container").innerHTML = "Radius cannot be blank"
+    return false
+  }
+  return true
 }
---
+++ NA/JS/Theo/basics.js
@@ -0,0 +1,6 @@
+console.log(a) //TypeError
+let a
+a = 5
+
+//let b
+
